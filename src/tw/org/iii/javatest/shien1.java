39724f119351f6615564881a4a194f70447eacc1
Liao <unidein@gmail.com>
2016-10-31 14:28:31 +0800

    test2

---
+++ src/tw/org/iii/javatest/shien1.java
@@ -0,0 +1,17 @@
+package tw.org.iii.javatest;
+public class shien1 {
+    public static void main(String[] args){
+        //integer:byte . short . int . long
+        byte Var1;  //[a-ZA-Z$_][a-zA-Z0-9$_]*
+        byte $_$;
+        byte 變數1;
+        $_$ = 127;
+        System.out.println($_$);
+        byte Var2 = -128;
+        Var2 -=1;
+        byte Var4 = 1;
+        int Var3 = Var2 + Var4 ;
+        int Var5 = 0x10; //012 0後面是八進位 0x後面十進位
+        System.out.println(Var5);
+    }
+}
---
+++ src/tw/org/iii/javatest/shine2.java
@@ -0,0 +1,10 @@
+package tw.org.iii.javatest;
+public class shine2 {
+    public static void main(String[] args) {
+        float Var1 = 12;
+        float Var2 = 12.3f;  //12.3 is double
+        double Var3 = 0, Var4 = 3;
+        int Var5 = 10, Var6 = 0;
+    System.out.println(Var3 / Var6);
+    }
+}
