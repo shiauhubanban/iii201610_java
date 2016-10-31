4eda4ed8a162b3554bb8cac89ffd8f6293495a18
Liao <unidein@gmail.com>
2016-10-31 16:37:09 +0800

    test5

--- src/tw/org/iii/javatest/shine7.java
+++ src/tw/org/iii/javatest/shine7.java
@@ -4,9 +4,9 @@ public class shine7 {
         System.out.println(1&3);
         int a = 10, b=3;
         if(++a <=10 && b-- >=3){ //(++a <=10 & b-- >=3) a=11,b=2;
-            System.out.println("OK:a= "+a+ ",b="+b);
+            System.out.println("OK:a = "+ a + ",b = "+ b);
         }else{
-            System.out.println("XX:a= "+a+ ",b="+b);
+            System.out.println("XX:a = "+ a + ",b = "+ b);
         }
     }
 }
