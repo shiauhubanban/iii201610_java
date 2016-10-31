795dcfe68eb95175b63fd7668f8f7d7b0233f301
Liao <unidein@gmail.com>
2016-10-31 16:33:44 +0800

    test4

---
+++ src/tw/org/iii/javatest/shine8.java
@@ -0,0 +1,32 @@
+package tw.org.iii.javatest;
+public class shine8 {
+    public static void main(String[] args){
+        int year = 100;
+        int month = 2; //int byte short char string
+        switch (month){
+            case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+                System.out.println("31");
+                break;
+            case 2:
+                if(year % 4 ==0) {
+                    if (year % 100 == 0) {
+                        if (year % 400 == 0) {
+                            System.out.println("29");
+                        } else {
+                            System.out.println("28");
+                        }
+                    } else {
+                        System.out.println("29");
+                    }
+                }else{
+                    System.out.println("28");
+                }
+                break;
+            case 4: case 6: case 9: case 11:
+                System.out.println("30");
+                break;
+
+
+        }
+    }
+}
