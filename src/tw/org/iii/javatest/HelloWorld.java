7de57406d5858a05638e2b1f66b6944b058dfabe
Liao <unidein@gmail.com>
2016-10-31 12:00:39 +0800

    test

--- src/tw/org/iii/javatest/HelloWorld.java
+++ src/tw/org/iii/javatest/HelloWorld.java
@@ -1,7 +1,7 @@
 package tw.org.iii.javatest;
 public class HelloWorld {
     public static void main(String[] brad){
-        System.out.println("Hello brad2");
+        System.out.println("Hello brad22");
         System.exit(0);
     }
 }
