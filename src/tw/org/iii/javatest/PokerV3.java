49e4971046db6673cc572ad6c3d01ee92fed5d9e
Liao <unidein@gmail.com>
2016-11-03 10:05:52 +0800

    20161103poker

--- src/tw/org/iii/javatest/PokerV3.java
+++ src/tw/org/iii/javatest/PokerV3.java
@@ -3,8 +3,26 @@ public class PokerV3 {
     public static void main(String[] args){
         //洗牌
         int[] poker = new int[52];
-        for(int i=0; i<poker.length;i++) poker[i]=i;
+        int Dealer ;
+        int i = 0;
+        int temp;
+        for( ; i<poker.length;i++) {
+            poker[i] = i;
+            System.out.print(i + " ");
+        }
+        System.out.println();
+            for (int j = 51; j >= 0; j--) {
+                temp = (int) (Math.random() * j);
+                Dealer = poker[temp];
+                poker[temp] =poker[j] ;
+                poker[j] = Dealer;
+                System.out.print(Dealer + " ");
+            }
+            System.out.println();
+
+
         //發牌
         //攤牌(理牌)
+        
     }
 }
