91b9ceee4e8b34328bc31091eae6f70c84542ecb
Liao <unidein@gmail.com>
2016-11-04 11:59:54 +0800

    20161104-substring&Regular

--- src/tw/org/iii/javatest/shine24substring.java
+++ src/tw/org/iii/javatest/shine24substring.java
@@ -1,13 +1,13 @@
 package tw.org.iii.javatest;
 public class shine24substring {
   public static void main(String[] args){
-      String id ="A123456789";
+      String id ="A223456789";
       String s1 = (id.substring(4));
       System.out.println(s1);
       String s2 = (id.substring(4,7));
       System.out.println(s2);
       System.out.println("----------");
-      if(id.matches("^[A-Z][01][0-9]{8}$")){
+      if(id.matches("^[A-Z][12][0-9]{8}$")){
           System.out.println("ok");
       }else{
           System.out.println("XX");
