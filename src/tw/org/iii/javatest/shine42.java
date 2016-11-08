f05b42470e4e0f52bc6056b6d89791e3f84f29b7
Liao <unidein@gmail.com>
2016-11-08 16:35:05 +0800

    20161108 net v3

--- src/tw/org/iii/javatest/shine42.java
+++ src/tw/org/iii/javatest/shine42.java
@@ -9,7 +9,7 @@ public class shine42 {
     public static void main(String[] args) {
             try {
                 Socket socket = new Socket(
-                        InetAddress.getByName("10.2.24."), 9999);
+                        InetAddress.getByName("10.2.24.255"), 9999);
                 socket.close();
                 System.out.println("ok");
             } catch (Exception ee) {
