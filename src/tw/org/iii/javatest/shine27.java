38a48cffaf8137a6bd56cbc0c95006398bc5e7ce
Liao <unidein@gmail.com>
2016-11-08 11:10:30 +0800

    20161108 window2

--- src/tw/org/iii/javatest/shine27.java
+++ src/tw/org/iii/javatest/shine27.java
@@ -4,10 +4,12 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.*;
 
 public class shine27 extends JFrame implements ActionListener{
     private JButton newFile, openFile, saveFile, saveAsFile, exit;
     private JTextArea editor;
+    private File editFile;
 
     public shine27(){ //只會執行第一次
         //super();
@@ -21,21 +23,38 @@ public class shine27 extends JFrame implements ActionListener{
         exit = new JButton("Exit");
         editor = new JTextArea();
 
-        newFile.addActionListener(new MyLister());
-        newFile.addActionListener(this);
+       // newFile.addActionListener(new MyLister());
+       // newFile.addActionListener(this);
         newFile.addActionListener(new ActionListener() {
               @Override
               public void actionPerformed(ActionEvent actionEvent) {
-                  System.out.println("C");
+                 newFile();  //按control可跳到所需位置
               }
           });
+
+        openFile.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                openFile();
+            }
+        });
+
+        saveFile.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                saveFile();
+            }
+        });
+
+
 
         JPanel top = new JPanel(new FlowLayout());
         top.add(newFile);top.add(openFile);top.add(saveFile);
         top.add(saveAsFile);top.add(exit);
 
         add(top,BorderLayout.NORTH);
-        add(editor, BorderLayout.CENTER);
+        JScrollPane jsp = new JScrollPane(editor);
+        add(jsp, BorderLayout.CENTER);
 
         setSize(640,480);
         setVisible(true);
@@ -44,6 +63,62 @@ public class shine27 extends JFrame implements ActionListener{
     }
     public static void main(String[] args){
         new shine27();
+    }
+    //產生新檔案
+    private void newFile (){
+        editFile = null;
+        editor.setText("");
+    }
+    //開起舊檔
+    private void openFile(){
+        JFileChooser jfc = new JFileChooser();
+        if(jfc.showOpenDialog(this) == JFileChooser.APPROVE_OPTION){
+            editFile = jfc.getSelectedFile();
+            loadFile();
+           // System.out.println(editFile.getAbsolutePath());
+        }
+    }
+    //讀檔案
+    private void loadFile(){
+        try {
+            StringBuffer sb = new StringBuffer();
+            BufferedReader br = new BufferedReader(new FileReader(editFile));
+            String line;
+            while ((line = br.readLine()) != null){
+                sb.append(line + "\n");
+            }
+            br.close();
+            editor.setText(sb.toString());
+        } catch (Exception e) {
+
+        }
+
+    }
+    //存檔
+    private void saveFile(){
+        if(editFile == null){
+             saveAsFile();
+        }else {
+            saveRealFile();
+        }
+    }
+
+    private void saveAsFile(){
+        JFileChooser jfc = new JFileChooser();
+        if (jfc.showSaveDialog(null) == JFileChooser.APPROVE_OPTION){
+            editFile = jfc.getSelectedFile();
+            saveRealFile();
+        }
+    }
+    private void saveRealFile(){
+        try{
+            BufferedWriter bw = new BufferedWriter(new FileWriter(editFile));
+            bw.write(editor.getText());
+            bw.flush();
+            bw.close();
+        }catch(IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
