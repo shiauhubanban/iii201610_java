349326f7959cefb2407f33b547f8a67a79630279
Liao <unidein@gmail.com>
2016-11-09 10:56:16 +0800

    20161109 net v5

--- src/tw/org/iii/javatest/shine42.java
+++ src/tw/org/iii/javatest/shine42.java
@@ -1,15 +1,25 @@
 package tw.org.iii.javatest;
 
+import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.Socket;
 
 public class shine42 {
-    //TCP
+    //TCP Client
     public static void main(String[] args) {
             try {
                 Socket socket = new Socket(
-                        InetAddress.getByName("10.2.24.255"), 9999);
+                        InetAddress.getByName("10.2.24.111"), 9999);
+
+                socket.getOutputStream();
+                BufferedOutputStream bout =
+                        new BufferedOutputStream(
+                                socket.getOutputStream());
+                bout.write("Hello, shine\nHello,World"  .getBytes());
+                bout.flush();
+                bout.close();
+
                 socket.close();
                 System.out.println("ok");
             } catch (Exception ee) {
--- src/tw/org/iii/javatest/shine43.java
+++ src/tw/org/iii/javatest/shine43.java
@@ -1,14 +1,34 @@
 package tw.org.iii.javatest;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class shine43 {
-    //TCP
+    //TCP Server
     public static void main(String[] args){
         try {
             ServerSocket server = new ServerSocket(9999);
-            server.accept();
+            System.out.println("Listening");
+            Socket socket = server.accept();
+            //取得IP位置
+            String urip = socket.getInetAddress().getHostAddress();
+            System.out.println(urip + ";" );
+
+            socket.getInputStream();
+
+            BufferedReader br =
+                    new BufferedReader(
+                            new InputStreamReader(
+                                    socket.getInputStream()));
+            String line;
+            while ((line= br.readLine()) != null){
+                System.out.println(line);
+            }
+            br.close();
+
             server.close();
             System.out.println("server OK");
         } catch (IOException e) {
