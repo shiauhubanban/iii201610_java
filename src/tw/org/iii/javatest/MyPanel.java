84d3bfeb69867a553e8715b905811f555d6b5ca9
Liao <unidein@gmail.com>
2016-11-11 16:15:06 +0800

    20161111 v6

--- src/tw/org/iii/javatest/MyPanel.java
+++ src/tw/org/iii/javatest/MyPanel.java
@@ -4,11 +4,15 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
+import java.util.HashMap;
+import java.util.LinkedList;
 
 public class MyPanel extends JPanel implements MouseMotionListener {  //類別
+     private LinkedList<HashMap<String ,Integer>> line;
 
      public MyPanel(){
-         addMouseMotionListener(this); 
+         addMouseMotionListener(this);
+         line = new LinkedList<>();
      }
 //        System.out.println("MyPaint()");
 //    }
@@ -21,15 +25,19 @@ public class MyPanel extends JPanel implements MouseMotionListener {  //類別
     @Override  //複寫
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        System.out.println("paintComponent()");
-
+        //System.out.println("paintComponent()");
         Graphics2D g2d = (Graphics2D)g;
-
         setBackground(Color.BLACK);
-
         g2d.setColor(Color.YELLOW);  //畫筆顏色
         g2d.setStroke(new BasicStroke(2)); //畫筆粗細
 
+        // line(4) =>0-1, 1-2, 2-3,
+        for(int i = 1; i<line.size(); i++){
+            HashMap<String,Integer> p0 = line.get(i-1);
+            HashMap<String,Integer> p1 = line.get(i);
+            g2d.drawLine(p0.get("x"),p0.get("y"),
+                    p1.get("x"),p1.get("y"));
+        }
        // g2d.drawOval(100,100, 40,40);
        // g2d.fillOval(200,200, 40,40);
         // g2d.drawLine(0,0, 300,300);
@@ -38,7 +46,11 @@ public class MyPanel extends JPanel implements MouseMotionListener {  //類別
     @Override
     public void mouseDragged(MouseEvent e) {
         int x = e.getX(); int y = e.getY();
-        System.out.println( x + " x " + y);  //抓點的位置
+        HashMap<String,Integer> point = new HashMap<>();  //建立出來的
+        point.put("x", x); point.put("y", y);  //放進去
+        line.add(point);
+        repaint();
+       // System.out.println( x + " x " + y);  //抓點的位置
     }
 
     @Override
