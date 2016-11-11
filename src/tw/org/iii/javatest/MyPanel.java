66c552b72f97f9226ebf2d984133f79d5a6983d6
Liao <unidein@gmail.com>
2016-11-11 14:54:32 +0800

    20161111 v4

--- src/tw/org/iii/javatest/MyPanel.java
+++ src/tw/org/iii/javatest/MyPanel.java
@@ -4,15 +4,29 @@ import javax.swing.*;
 import java.awt.*;
 
 public class MyPanel extends JPanel {  //類別
+//    public MyPanel(){
+//        System.out.println("MyPaint()");
+//    }
+//    @Override
+//    public void paint(Graphics g) {
+//        super.paint(g);
+//        System.out.println("paint()");
+//    }
+
     @Override  //複寫
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
+        System.out.println("paintComponent()");
+
         Graphics2D g2d = (Graphics2D)g;
 
-        g2d.setColor(Color.RED);  //畫筆顏色
-        g2d.setStroke(new BasicStroke(4)); //畫筆粗細
+        setBackground(Color.BLACK);
 
-        g2d.drawOval(100,100, 40,40);
-        g2d.fillOval(200,200, 40,40);
+        g2d.setColor(Color.YELLOW);  //畫筆顏色
+        g2d.setStroke(new BasicStroke(2)); //畫筆粗細
+
+       // g2d.drawOval(100,100, 40,40);
+       // g2d.fillOval(200,200, 40,40);
+        g2d.drawLine(0,0, 300,300);
     }
 }
