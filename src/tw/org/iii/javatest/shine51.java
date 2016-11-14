c068f61a30b418b5120320f6f7c4f61e27470861
Liao <unidein@gmail.com>
2016-11-14 15:40:07 +0800

    20161114 v6

--- src/tw/org/iii/javatest/shine51.java
+++ src/tw/org/iii/javatest/shine51.java
@@ -3,6 +3,11 @@ public class shine51 {
     public static void main(String[] args){
         MyThread mt1 = new MyThread("A");
         MyThread mt2 = new MyThread("B");
+
+        MyRunnable mr1 = new MyRunnable("C");
+        Thread t1 = new Thread(mr1);  //執行緒
+        t1.start();
+
         mt1.start();mt2.start();  //同個start不可以啟動兩次 會拋出錯誤
         System.out.println("Game Over");
     }
@@ -14,6 +19,21 @@ class MyThread extends Thread{  //跟物件相同等級
     @Override
     public void run() {
         super.run();
+        for (int i = 0 ;i<20; i++){
+            System.out.println(name +":"+ i);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException ie) {
+
+            }
+        }
+    }
+}
+class MyRunnable implements Runnable{  //不是執行緒
+    private String name;
+    MyRunnable(String name){this.name = name;}
+    @Override
+    public void run() {
         for (int i = 0 ;i<20; i++){
             System.out.println(name +":"+ i);
             try {
